68ff2ed16185d8562e9d1cd68ab115b6c766ac28
Alexandre Boyer <alexandreboyer39@gmail.com>
2023-04-18 01:39:27 +0200

    fixing a path bugs

--- view/accueil.php
+++ view/accueil.php
@@ -60,7 +60,7 @@
                 <h2 data-aos="fade-up" data-aos-delay="400">Oasys consulting</h2>
                 <div data-aos="fade-up" data-aos-delay="600">
                     <div class="text-center text-lg-start">
-                        <a href="../controller/route.php?action=demander_connexion" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
+                        <a href="../controller/Route.php?action=demander_connexion" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
                             <span>Se connecter</span>
                             <i class="bi bi-arrow-right"></i>
                         </a>
